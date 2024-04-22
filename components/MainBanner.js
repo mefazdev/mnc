9a84b37039778595392e49c7abe39a43655f92f8
mefazdev <fazdev.com@gmail.com>
2024-04-22 19:41:17 +0530

    aertr

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -26,7 +26,6 @@ export default function MainBanner({poster}) {
       <div className="md:hidden">
         <MobBanner />
       </div>
-<<<<<<< HEAD
       <div className="banner__carousel  relative hidden md:grid bg-black bg-opacity-90">
         <Carousel
           // autoPlay={false}
@@ -116,94 +115,12 @@ export default function MainBanner({poster}) {
                   </button>
                 </div>
               </div>
-=======
-<div className="banner__carousel hidden md:grid bg-black bg-opacity-90">
-      <Carousel
-        // autoPlay={false}
-        indicators={false}
-        infiniteLoop={true}
-        stopAutoPlayOnHover={false}
-      >
-        <div className="banner__img bg-black ">
-          <div className="main__banner__text ">
-            <div className="main__banner__text__first__row">
-              <h2 className="text-center  w-full">
-                {/* <span className="font-normal ">WELCOME TO</span> */}
-                JAMIA MADEENATHUNNOOR
-              </h2>
-            </div>
-            <div className="main__banner__text__second__row">
-              <h3 className="text-center  w-full">
-                PIONEERING A PROMISING FUTURE
-              </h3>
-              <h6 className="text-center">GET AN ADMISSION</h6>
-              <div className="grid grid-cols-2 gap-4">
-                <button className="m-auto rounded w-full hover:scale-105 hover:transition ease-linear duration-150 hover:border border-white">
-                  <a  href="https://docs.google.com/forms/d/e/1FAIpQLSd3JChgrFpRN5QebID-_nFwBT0lpCrJqmwEbgdbAslV7mBdEg/viewform?usp=sf_link">
-                    JMN JUNIOR SCHOOL
-                  </a>{" "}
-                </button>
-              {/* </div> */}
-
-              <button className="ml- w-full hover:scale-105 hover:transition ease-linear duration-150 hover:border border-white"><a href="https://admission.jamiamadeenathunnoor.org">JMN SENIOR SCHOOL</a> </button>
-            </div>
-            </div>
-          </div>
-        </div>
-        <div className="banner__img__three">
-          <div className="main__banner__text">
-            <div className="main__banner__text__first__row">
-              <h2 className="text-center  w-full">JAMIA MADEENATHUNNOOR</h2>
-            </div>
-            <div className="main__banner__text__second__row">
-              <h3 className="text-center  w-full">
-                PIONEERING A PROMISING FUTURE
-              </h3>
-              <h6 className="text-center">GET AN ADMISSION</h6>
-              <div className="grid grid-cols-2 gap-4">
-                <button className="m-auto rounded w-full hover:scale-105 hover:transition ease-linear duration-150 hover:border border-white">
-                  <a  href="https://docs.google.com/forms/d/e/1FAIpQLSd3JChgrFpRN5QebID-_nFwBT0lpCrJqmwEbgdbAslV7mBdEg/viewform?usp=sf_link">
-                   JMN JUNIOR SCHOOL
-                  </a>{" "}
-                </button>
-              {/* </div> */}
-
-              <button className="ml- w-full hover:scale-105 hover:transition ease-linear duration-150 hover:border border-white"><a href="https://admission.jamiamadeenathunnoor.org">JMN SENIOR SCHOOL</a> </button>
-            </div>
-            </div>
-          </div>
-        </div>
-        <div className="banner__img__two">
-          <div className="main__banner__text">
-            <div className="main__banner__text__first__row">
-              <h2 className="text-center  w-full">
-                {/* <span className="font-normal ">WELCOME TO</span> */}
-                JAMIA MADEENATHUNNOOR
-              </h2>
-            </div>
-            <div className="main__banner__text__second__row">
-              <h3 className="text-center  w-full">
-                PIONEERING A PROMISING FUTURE
-              </h3>
-              <h6 className="text-center">GET AN ADMISSION</h6>
-              <div className="grid grid-cols-2 gap-4">
-                <button className="m-auto   w-full hover:scale-105 hover:transition ease-linear duration-150 hover:border border-white">
-                  <a  href="https://docs.google.com/forms/d/e/1FAIpQLSd3JChgrFpRN5QebID-_nFwBT0lpCrJqmwEbgdbAslV7mBdEg/viewform?usp=sf_link">
-                  JMN JUNIOR SCHOOL
-                  </a>{" "}
-                </button>
-              {/* </div> */}
-
-              <button className="ml- w-full hover:scale-105 hover:transition ease-linear duration-150 hover:border border-white"><a href="https://admission.jamiamadeenathunnoor.org">JMN SENIOR SCHOOL</a> </button>
-            </div>
->>>>>>> 5f98d0b96ce66be9efcf24fc538cc152ff834a14
             </div>
           </div>
         </Carousel>
         <Poster poster={poster}/>
 
         <Modal open={open} id="modal">
-<<<<<<< HEAD
           <div className="modal border-4 ">
             <div className="modal__close">
               <CloseIcon id="close__icon" onClick={() => setOpen(false)} />
@@ -229,22 +146,6 @@ export default function MainBanner({poster}) {
                   </button>
                 </a>
               </div>
-=======
-        <div className="modal border-4 ">
-          <div className="modal__close">
-            <CloseIcon id="close__icon" onClick={() => setOpen(false)} />
-          </div>
-          <div className="modal__main pt-4 pb-4">
-            <h2>GET AN ADMISSION</h2>
-            {/* <h3>Science Academy - (8th Std.)</h3> */}
-            <div className="modal__btn lg:flex">
-            <a className="flex mt-2 w-full"  href="https://docs.google.com/forms/d/e/1FAIpQLSd3JChgrFpRN5QebID-_nFwBT0lpCrJqmwEbgdbAslV7mBdEg/viewform?usp=sf_link">
-                   <button className="m-auto border hover:scale-105 hover:transition ease-linear duration-150 hover:border border-gray-300">JMN JUNIOR SCHOOL</button>
-              </a>
-              <a className="flex mt-2 w-full" href="https://admission.jamiamadeenathunnoor.org">
-              <button className="m-auto border border-gray-200  hover:scale-105 hover:transition ease-linear duration-150 hover:border ">JMN SENIOR SCHOOL</button>
-              </a>
->>>>>>> 5f98d0b96ce66be9efcf24fc538cc152ff834a14
             </div>
           </div>
         </Modal>
