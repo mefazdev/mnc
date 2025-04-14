c0dd53d6fc32e8c12c3e4d1630c7357a255393b9
mefazdev <fazdev.com@gmail.com>
2025-04-14 17:43:11 +0530

    Fix updates changed node v

--- components/Footer.js
+++ components/Footer.js
@@ -1,153 +1,4 @@
-// import React from "react";
-// import SchoolIcon from "@material-ui/icons/School";
-// import CallIcon from "@material-ui/icons/Call";
-// import MailIcon from "@material-ui/icons/Mail";
-// import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-// import InstagramIcon from "@material-ui/icons/Instagram";
-// import FacebookIcon from "@material-ui/icons/Facebook";
-// import TwitterIcon from "@material-ui/icons/Twitter";
-// import YouTubeIcon from "@material-ui/icons/YouTube";
-// import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
-// import Link from "next/link";
-// export default function Footer() {
-//   return (
-//     <div className="footer bg-primary">
-//       <div className="footer__content lg:gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-//         <div className="footer__address">
-//           <div className="flex">
-//             <SchoolIcon id="footer__icon" />
-//             <div className="pl-2">
-//               <h5>JAMIA MADEENATHUNNOOR</h5>
-//               <h6>
-//                 Poonoor, Unnikulam(PO), Kozhikode, Kerala
-//                 <br /> India - PIN: 673 574
-//               </h6>
-//               {/* <h5>CIN : U72900KL2016PTC047206</h5> */}
-//             </div>
-//           </div>
-//           <div className="flex footer__call">
-//             <CallIcon id="footer__icon" />
-//             <p>0495 2963484</p>
-//           </div>
-//           <div className="flex footer__mail">
-//             <MailIcon id="footer__icon" />
-//             <p>info@jamiamadeenathunoor.org</p>
-//           </div>
-//           <div className="flex footer__social">
-//             <a href="https://www.facebook.com/profile.php?id=100085673749100">
-//               <FacebookIcon id="footer__social__icon" />
-//             </a>
-
-//             <a href="https://www.instagram.com/jamia_madeenathunnoor/">
-//               <InstagramIcon id="footer__social__icon" />
-//             </a>
-//             <a>
-//               <TwitterIcon id="footer__social__icon" />
-//             </a>
-
-//             <a href="https://www.youtube.com/c/GlocalMediaMarkazGarden">
-//               <YouTubeIcon id="footer__social__icon" />
-//             </a>
-//           </div>
-//           <div className="flex footer__input">
-//             <input placeholder="Subscribe our newsletter" />
-//             <span className="rounded-full">
-//               <ArrowForwardIcon id="footer__arrow" />
-//             </span>
-//           </div>
-//         </div>
-
-//         <div className="footer__menu lg:m-auto mt-10    lg:mt-0 ">
-//           <Link passHref href="/Home">
-//             <h6>Home</h6>
-//           </Link>
-//           <Link passHref  href="/About">
-//             <h6>About</h6>
-//           </Link>
-
-//           {/* <Link href='/Blog'>
-//              <h6>News</h6>
-//              </Link> */}
-
-//           {/* <Link href='/News'><h6>Events</h6></Link> */}
-//           <a href="https://manager.jamiamadeenathunnoor.org/">
-//             <h6>Staff Panel</h6>
-//           </a>
-//           <Link passHref href="/Contact">
-//             <h6>Contact us</h6>
-//           </Link>
-//         </div>
-//         <div className="footer__menu lg:m-auto   lg:mt-0 ">
-//           <Link passHref href="/Programmes">
-//             <h6>Academics</h6>
-//           </Link>
-//           <Link  passHref href="/Faculty">
-//             <h6>Faculty profile</h6>
-//           </Link>
-
-//           <h6>Admission</h6>
-//           <Link passHref href="/About">
-//             <h6>Vision & Mission</h6>
-//           </Link>
-
-//           {/* <Link href='/'><h6>Message of rector</h6></Link> */}
-//           {/* <Link href='/'><h6>Message of Chancellor</h6></Link> */}
-//         </div>
-//         <div className="footer__menu lg:m-auto  lg:mt-0 ">
-//           {" "}
-//           <Link passHref href="/campuses/OnCampuses">
-//             <h6>On-Campuses</h6>
-//           </Link>
-
-//           <Link passHref href="/campuses/ScienceCampuses">
-//             <h6>Science  Campuses</h6>
-//           </Link>
-
-//           <Link passHref href="/campuses/Erthunnabavi">
-//             <h6>Erthunnabavi Campuses</h6>
-//           </Link>
-       
-         
-//           <Link href="/admin/Login">
-//             <h6>Admin</h6>
-//           </Link>
-        
-//         </div>
-//       </div>
-
-//       <div className="footer___bottom  bg-white/10">
-//         <div className="footer__bottom__content flex">
-//           {/* <div className='flex footer__bottom__left'>
-//       <Link href='/'><p>Home</p></Link>
-    
-//     <p className='ml-1'>|</p>
-//     <Link href='/About'><p className='ml-1'>About </p></Link>
-    
-//     <p className='ml-1'>|</p>
-//     <Link href='/Contact'><p className='ml-1'>Contact</p></Link>
-    
-//     </div> */}
-//           <div className="footer__bottom__right">
-//             <p>
-//               ©2025. Powered by{" "}
-//               <a
-//                 href="whatsapp://send?phone=+918606256402"
-//                 data-action="share/whatsapp/share"
-//                 style={{ textDecoration: "underline", cursor: "pointer" }}
-//               >
-//                 {/* <p style={{textDecoration:'underline',cursor:'pointer'}} className="ml-2"> */}
-//                 Booreact
-//                 {/* </p> */}
-//               </a>
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
+ 
 import React from 'react';
 import Image from 'next/image';
 import { FaMapMarkerAlt,  FaPhone, FaEnvelope, FaClock, FaFacebook, FaTwitter, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa';
@@ -159,7 +10,7 @@ const Footer = () => {
     { name: "About", url: "/About" },
     { name: "Staff Panel", url: "https://manager.jamiamadeenathunnoor.org/" },
     { name: "Contact us", url: "/Contact" },
-    { name: "Programmes", url: "/Crogrammes" },
+    { name: "Programmes", url: "/Programmes" },
     { name: "On-Campuses", url: "/campuses/OnCampuses" },
     { name: "Science Academies", url: "/campuses/Science" },
     { name: "Erthunnabavi Campuses", url: "/campuses/Erthunnabavi" },
@@ -228,8 +79,8 @@ const Footer = () => {
             <h3 className="text-lg font-bold mb-4 border-b border-gray-700 pb-2">Quick Links</h3>
             <div className="grid grid-cols-2 gap-2">
               {quickLinks.map((link, index) => (
-                link.name !== "Staff Panel" ? <span className='text-white hover:text-white/80'> <Link
-                key={index}
+                link.name !== "Staff Panel" ? <span key={link.name} className='text-white hover:text-white/80'> <Link
+                
                 href={link.url}
                 className="  transition-colors"
               >
